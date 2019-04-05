0851cc75abaab87212faf5af83b4a5ceac395eab
Tobias C. Berner <tcberner@FreeBSD.org>
2019-04-06 01:47:06 +0200

    applications: multimedia/kdenlive: update to 19.04-rc

--- multimedia/kdenlive/files/patch-src_lib_external_media__ctrl_mediactrl.c
+++ multimedia/kdenlive/files/patch-src_lib_external_media__ctrl_mediactrl.c
@@ -4,13 +4,7 @@ Date: Sun, 24 Mar 2019 08:53:10 +0100
 Subject: [PATCH] Remove duplicate include of asm/types.h
 
 - it is only required on Linux and already included below.
----
- src/lib/external/media_ctrl/mediactrl.c | 1 -
- 1 file changed, 1 deletion(-)
-
-diff --git src/lib/external/media_ctrl/mediactrl.c src/lib/external/media_ctrl/mediactrl.c
-index d7fb0f0a9..55039a718 100644
---- src/lib/external/media_ctrl/mediactrl.c
+--- src/lib/external/media_ctrl/mediactrl.c.orig	2019-04-04 17:11:51 UTC
 +++ src/lib/external/media_ctrl/mediactrl.c
 @@ -18,7 +18,6 @@
   * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
@@ -20,6 +14,3 @@ index d7fb0f0a9..55039a718 100644
  #include <errno.h>
  #include <fcntl.h>
  #include <stdint.h>
--- 
-2.21.0
-
